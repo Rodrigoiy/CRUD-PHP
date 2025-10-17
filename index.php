bb5dfc93245c50ee031c7f88deee31a633bf3214
Rodrigoiy <rodrigoissamuyakushijin@gmail.com>
2025-10-17 05:31:55 -0300

    alteração no acoes.php, criado a criptografia de senha utilizando password_hash

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require 'conexao.php';
 ?>
 
 <!doctype html>
-<html>
+<html lang="pt-br">
   <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
